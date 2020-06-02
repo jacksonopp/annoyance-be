8a6cd1c983d94037ba0adff1636ae1bddbf8edbf
Jackson Oppenheim <jacksonop@gmail.com>
2020-06-02 14:59:20 -0500

    cors hopefully

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ db.on('error', (err) => {
   console.log(err);
 });
 
-app.use(cors());
+app.use(cors({ origin: 'https://annoyance.netlify.app' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
